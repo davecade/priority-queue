bd8db929cc2e47121c66d07ef9e62266d2faeb6b
Dave <dave.cadelina@outlook.com>
2021-07-20 07:16:07 +1000

    changed search style

--- src/components/SearchBar/SearchBar.component.jsx
+++ src/components/SearchBar/SearchBar.component.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './SearchBar.styles.scss'
 
 const SearchBar = () => {
+
     return (
-        <div className="search-container">
+        <div className="search-container" >
             <input type="text" className="search-bar" placeholder="Search..."/>
-            <i class="fas fa-search"></i>
         </div>
     )
 }
