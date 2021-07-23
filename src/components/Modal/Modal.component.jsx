5c532239425bfa75ecb1bf0d2301c8d634975cdb
Dave <dave.cadelina@outlook.com>
2021-07-23 11:48:13 +1000

    added internal useState for modal

--- src/components/Modal/Modal.component.jsx
+++ src/components/Modal/Modal.component.jsx
@@ -4,20 +4,61 @@ import { connect } from 'react-redux'
 import { disableModal } from '../../Redux/modal/modal.actions'
 
 const Modal = ({ modalEnabled, disableModal }) => {
+    //-- Modal State
     const [ visibility, setVisibility ] = useState("hidden")
+    const [ opacity, setOpacity ] = useState("0")
+    const [ top, setTop ] = useState("4%")
+
+    //--Current Input Values
+    const [ issue, setIssue ] = useState('')
+    const [ reporter, setReporter ] = useState('Unassigned')
+    const [ description, setDescription ] = useState('')
+    const [ priority, setPriority ] = useState("Low")
 
     useEffect(() => {
         if(modalEnabled) {
             setVisibility("visible")
+            setOpacity('1')
+            setTop('8.5%')
         } else {
             setVisibility("hidden")
+            setOpacity('0')
+            setTop('4%')
+            setIssue('')
+            setReporter('Unassigned')
+            setDescription('')
+            setPriority("Low")
         }
     }, [modalEnabled])
 
+    const handleIssueChange = event => {
+        setIssue(event.target.value)
+    }
+
+    const handleReporterChange = event => {
+        setReporter(event.target.value)
+    }
+
+    const handleDescriptionChange = event => {
+        setDescription(event.target.value)
+    }
+
+    const handlePriorityChange = event => {
+        setPriority(event.target.value)
+    }
+
+
     return (
         <Fragment>
-            <div className="modal-background" style={ { visibility: visibility } }>
-                <div className="modal" style={ { visibility: visibility } }>
+            <div className="modal-background" style={{
+                visibility: visibility,
+                opacity: opacity,
+            }}>
+                <div className="modal" style={{
+                    visibility: visibility,
+                    opacity: opacity,
+                    top: top
+                }}>
                     <div className="modal-content">
                         <h1 className="modal-title">Create New Ticket</h1>
 
@@ -37,23 +78,24 @@ const Modal = ({ modalEnabled, disableModal }) => {
                             </div>
 
                             <div className="issue">
-                                <input type="text" />
+                                <input type="text" onChange={handleIssueChange} value={issue} />
                             </div>
                             <div className="reporter">
-                                <select name="" id="">
-                                    <option value="">Caity</option>
-                                    <option value="">Coco</option>
-                                    <option value="">Cruezian</option>
+                                <select onChange={handleReporterChange} value={reporter}>
+                                    <option value="Unassigned">Unassigned</option>
+                                    <option value="Caity">Caity</option>
+                                    <option value="Coco">Coco</option>
+                                    <option value="Cruezian">Cruezian</option>
                                 </select>
                             </div>
                             <div className="description">
-                                <textarea id="w3review" name="w3review" rows="10" cols="50" />
+                                <textarea onChange={handleDescriptionChange} value={description} rows="10" cols="50" />
                             </div>
                             <div className="priority">
-                                <select name="" id="">
-                                    <option value="">Low</option>
-                                    <option value="">Medium</option>
-                                    <option value="">High</option>
+                                <select onChange={handlePriorityChange} value={priority}>
+                                    <option value="Low">Low</option>
+                                    <option value="Medium">Medium</option>
+                                    <option value="High">High</option>
                                 </select>
                             </div>
                         </div>
