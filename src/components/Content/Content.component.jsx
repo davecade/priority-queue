56c6b090f62781c7a01f97e5605f701754da4a56
Dave <dave.cadelina@outlook.com>
2021-07-25 10:49:21 +1000

    added ticket page using react router

--- src/components/Content/Content.component.jsx
+++ src/components/Content/Content.component.jsx
@@ -2,17 +2,22 @@ import React, { useEffect } from 'react'
 import './Content.styles.scss'
 import MainPage from '../../Pages/Main/Main.page'
 import { connect } from 'react-redux'
+import { Switch, Route } from 'react-router-dom'
+import Ticket from '../../Pages/Ticket/Ticket.page'
 
 const Content = ({ selectedTicket }) => {
 
     useEffect(() => {
         console.log("selectedTicket", selectedTicket)
-        
+
     }, [selectedTicket])
 
     return (
         <div className="content-container">
-            <MainPage />
+            <Switch>
+                <Route exact path='/' component={MainPage} />
+                <Route path='/Ticket' component ={Ticket} />
+            </Switch>
         </div>
     )
 }
