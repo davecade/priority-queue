56a01db201cbce7306e1ad49b255c681cdd74edd
Dave <dave.cadelina@outlook.com>
2021-08-07 12:57:20 +1000

    changed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import './App.scss';
-import Navbar from './components/Navbar/Navbar.component'
 import Content from './components/Content/Content.component'
 import CreateModal from './components/Modal/Create/Create-Modal.component'
 import EditModal from './components/Modal/Edit/Edit-Modal.component'
 import AssignModal from './components/Modal/Assign/Assign-Modal.component'
+import Navbar from './components/Navbar/Navbar.component'
 import { connect } from 'react-redux';
 import { fetchTicketDataStart } from './Redux/tickets/ticket.actions'
 import { fetchUserDataStart } from './Redux/users/user.actions'
